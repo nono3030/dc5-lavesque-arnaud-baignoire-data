575962a5984621035b2fb0300d22300832f3b532
nlmt <arnaud.lavesque@gmail.com>
2023-11-06 09:36:18 +0100

    ex3-a

---
+++ ex3-a.py
@@ -0,0 +1,12 @@
+cout_campagne = 5000
+revenu_genere = 8000
+
+
+if revenu_genere > cout_campagne:
+    resultat = "La campagne a été rentable."
+else:
+    resultat = "La campagne n'a pas été rentable."
+
+
+print(resultat)
+
