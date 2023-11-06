0d11088a957190b2047bbd2521f58603e2b7d85e
nlmt <arnaud.lavesque@gmail.com>
2023-11-06 09:06:28 +0100

    ex2-a ex2-a

---
+++ ex2-a.py
@@ -0,0 +1,9 @@
+
+depenses_mensuelles = [1000, 1200, 800, 900, 1100, 950, 1050, 1000, 950, 1100, 1200, 800]
+
+depenses_annuelles = 0
+
+for depense in depenses_mensuelles:
+    depenses_annuelles += depense
+
+print("Les dépenses totales de l'année sont de", depenses_annuelles, "euros.")
