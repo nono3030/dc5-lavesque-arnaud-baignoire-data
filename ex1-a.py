a48c2a899eb3d554f7c53601ee9f4203ed7094cc
nlmt <arnaud.lavesque@gmail.com>
2023-11-06 08:54:49 +0100

    exo-1-a

---
+++ ex1-a.py
@@ -0,0 +1,10 @@
+nom_entreprise = "Ma Société"
+nb_clients = 100
+revenu_moyen_par_client = 500
+
+revenu_total = nb_clients * revenu_moyen_par_client
+
+print("Nom de l'entreprise :", nom_entreprise)
+print("Nombre de clients :", nb_clients)
+print("Revenu moyen par client :", revenu_moyen_par_client)
+print("Revenu total :", revenu_total)
